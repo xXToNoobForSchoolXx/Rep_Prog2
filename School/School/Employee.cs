8d42dd0c39e8cdaa835239e87b2b679f0cb9c30c
xXToNoobForSchoolXx <114475364+xXToNoobForSchoolXx@users.noreply.github.com>
2022-12-09 13:33:24 +0100

    Add asignnment

---
+++ School/School/Employee.cs
@@ -0,0 +1,96 @@
+﻿///<summary>
+/// Namn: Simon Gottfridsson
+/// Klass: SY21
+/// Info:
+/// Sparar värden för de olika anställda.
+/// </summary>
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace School
+{
+    /// <summary>
+    /// Employee är en superklass med två variabler liksom två subklasser.
+    /// </summary>
+    abstract class Employee
+    {
+        /// <value>
+        /// Variabeln Name sparar den anställdes namn.
+        /// </value>
+        protected string Name { get; set; }
+        /// <value>
+        /// Variabeln SSN sparar den anställdes personnummer.
+        /// </value>
+        protected string SSN { get; set; }
+        /// <summary>
+        /// Konstruktorn för Employee.
+        /// </summary>
+        /// <param name="name"> Innehåller namn. </param>
+        /// <param name="sSN"> Innehåller personnummer. </param>
+        public Employee(string name, string sSN)
+        {
+            Name = name;
+            SSN = sSN;
+        }
+
+        public abstract string getInfo();
+    }
+    /// <summary>
+    /// Subklassen "Teacher" sparar data för anstälda lära.
+    /// Klassen har en extra variabel.
+    /// </summary>
+    class Teacher : Employee
+    {
+        /// <value>
+        /// Variabeln Subjects sparar lärarens ämnen.
+        /// </value>
+        private string Subjects { get; set; }
+        /// <summary>
+        /// Konstruktorn för Teacher.
+        /// </summary>
+        /// <param name="name"> Innehåller namn. </param>
+        /// <param name="sSN"> Innehåller personnummer. </param>
+        /// <param name="subjects"> Innehåkker lärarens "lärande" ämnen. </param>
+        public Teacher(string name, string sSN, string subjects) : base(name, sSN)
+        {
+            Subjects = subjects;
+        }
+
+        /// <summary>
+        /// Metoden ger ut data i consollen.
+        /// </summary>
+        /// <returns> Alla variabler i subklassen. </returns>
+        public override string getInfo()
+        {
+            return "Namn:" + Name + " Personnummer: " + SSN + " Subjects: " + Subjects;
+        }
+    }
+
+    /// <summary>
+    /// Subklassen "Principal" sparar data för anställda rektorer.
+    /// </summary>
+    class Principal : Employee
+    {
+        /// <summary>
+        /// Konstruktorn för Principal.
+        /// </summary>
+        /// <param name="name"> Innehåller namn. </param>
+        /// <param name="sSN"> Innehåller personnummer. </param>
+        public Principal(string name, string sSN) : base(name, sSN)
+        {
+
+        }
+
+        /// <summary>
+        /// Metoden ger ut data i consollen.
+        /// </summary>
+        /// <returns> Alla variabler i subklassen. </returns>
+        public override string getInfo()
+        {
+            return "Namn: " + Name + " Personnummer: " + SSN;
+        }
+    }
+}
---
+++ School/School/Program.cs
@@ -0,0 +1,98 @@
+﻿///<summary>
+/// Namn: Simon Gottfridsson
+/// Klass: SY21
+/// Info:
+/// Programmet låter användaren att skriva in x-antal anställda som sedan vissas i programmets slut.
+/// </summary>
+namespace School
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employees = new List<Employee>();
+
+            string name;
+            string sSN;
+
+            while (true)
+            {
+                Console.WriteLine("(L)ärare / (C)hef / (A)vsluta");
+                string ans = Console.ReadLine().ToLower();
+
+                if (ans == "a")
+                {
+                    break;
+                }
+
+                while (true)
+                {
+                    Console.WriteLine("Namn:");
+                    name = Console.ReadLine();
+
+                    if (name != null)
+                    {
+                        break;
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Felaktig input");
+                    }
+                }
+
+                while (true)
+                {
+                    Console.Write("Personnummer: ");
+                    sSN = Console.ReadLine();
+
+                    if (sSN != null)
+                    {
+                        break;
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Felaktig input");
+                    }
+                }
+
+                if (ans == "l")
+                {
+                    string subjects;
+
+                    while (true)
+                    {
+
+                        Console.WriteLine("Ämnen: ");
+                        subjects = Console.ReadLine();
+
+                        if (subjects != null)
+                        {
+                            break;
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("Felaktig input");
+                        }
+                    }
+
+                    employees.Add(new Teacher(name, sSN, subjects));
+                }
+
+                if (ans == "c")
+                {
+                    employees.Add(new Principal(name, sSN));
+                }
+            }
+
+            foreach (Employee employee in employees)
+            {
+                Console.WriteLine(employee.getInfo(), Console.Read());
+                Console.WriteLine("\nProgrammet stängs av om 10 sekunder.");
+                System.Threading.Thread.Sleep(100000);
+            }
+        }
+    }
+}
